56e5a9b5cde3e50d9935d29c88159bdc83fc2228
gaochenglong <1193358079@qq.com>
2021-10-12 14:35:56 +0800

    人资项目初始化

---
+++ src/settings.js
@@ -0,0 +1,18 @@
+module.exports = {
+
+  title: 'Vue Admin Template',
+
+  /**
+   * @type {boolean} true | false
+   * @description Whether fix the header
+   * 是否固定头部
+   */
+  fixedHeader: false,
+
+  /**
+   * @type {boolean} true | false
+   * @description Whether show the logo in sidebar
+   * 是否显示侧边栏的logo
+   */
+  sidebarLogo: false
+}
